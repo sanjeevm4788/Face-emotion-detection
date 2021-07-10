02a567b8a770363ed8b86de6c8003480e3dc86bd
Sanjeev <60375858+sanjeevm4788@users.noreply.github.com>
2021-07-10 21:43:42 +0530

    Update mainpage.py

--- src/mainpage.py
+++ src/mainpage.py
@@ -6,4 +6,6 @@ def mainpage():
     st.write("It can detect 7 emotions.")
     st.write('Angry :angry:, Disgust :persevere:, Fear 😨, Happy :smile:, Sad :worried:, Suprise :astonished:, Neutral :expressionless:.')
     st.subheader('Deep learning with Webcam and Deep learning with picture-')
-    st.text('They both use pre-trained deep learning model to predict the face emotion in picture and in live webcam.')
+    st.text('They both use deep learning model to predict the face emotion in picture and in live webcam.')
+    st.text('You can use the deep learning with webcam to detect the facial emotion on real time through your webcam.')
+    st.text('You can use the deep learning with picture to detect facial emotion on the uploaded picture.')
